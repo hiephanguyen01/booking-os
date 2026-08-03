cec5f095e157be38fce18ec0bdbceb3a3a86c628
Nguyen Ha Hiep <74894051+hiephanguyen01@users.noreply.github.com>
2026-08-03 20:49:31 +0700

    feat(api-client): export typed client API

---
+++ packages/api-client/src/index.ts
@@ -0,0 +1,10 @@
+export {
+  createApiClient,
+  type ApiClient,
+  type ApiClientOptions,
+} from "./client.js";
+export {
+  ApiClientError,
+  type ApiClientErrorCode,
+  type ApiClientErrorOptions,
+} from "./errors.js";
