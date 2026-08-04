3d73c090251da7617ae86ca72201e95c2a688160
Nguyen Ha Hiep <74894051+hiephanguyen01@users.noreply.github.com>
2026-08-04 11:59:22 +0700

    feat(api): register database and tenancy foundation

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from "@nestjs/common";
 
 import { RequestContextModule } from "./common/request-context/request-context.module.js";
 import { EnvironmentModule } from "./config/environment.module.js";
+import { DatabaseModule } from "./database/database.module.js";
 import { HealthModule } from "./health/health.module.js";
+import { TenancyModule } from "./tenancy/tenancy.module.js";
 
 @Module({
-  imports: [EnvironmentModule, RequestContextModule, HealthModule],
+  imports: [
+    EnvironmentModule,
+    RequestContextModule,
+    DatabaseModule,
+    HealthModule,
+    TenancyModule,
+  ],
 })
 export class AppModule {}
