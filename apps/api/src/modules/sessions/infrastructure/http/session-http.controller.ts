84bd391b213318d3d85e222de4cf2484128f9b80
Nguyen Ha Hiep <74894051+hiephanguyen01@users.noreply.github.com>
2026-08-06 16:29:30 +0700

    feat: add injectable session HTTP controller

---
+++ apps/api/src/modules/sessions/infrastructure/http/session-http.controller.ts
@@ -0,0 +1,30 @@
+import { Controller } from "@nestjs/common";
+
+import { RequestContextStorage } from "../../../../common/request-context/request-context.storage.js";
+import { EnvironmentService } from "../../../../config/environment.service.js";
+import { ListSessionsUseCase } from "../../application/use-cases/list-sessions.js";
+import { LoginUseCase } from "../../application/use-cases/login.use-case.js";
+import { RevokeOtherSessionsUseCase } from "../../application/use-cases/revoke-other-sessions.js";
+import { RevokeSessionUseCase } from "../../application/use-cases/revoke-session.js";
+import { AuthController } from "./auth.controller.js";
+
+@Controller("auth")
+export class SessionHttpController extends AuthController {
+  constructor(
+    loginUseCase: LoginUseCase,
+    requestContext: RequestContextStorage,
+    environment: EnvironmentService,
+    revokeSessionUseCase: RevokeSessionUseCase,
+    listSessionsUseCase: ListSessionsUseCase,
+    revokeOtherSessionsUseCase: RevokeOtherSessionsUseCase,
+  ) {
+    super(
+      loginUseCase,
+      requestContext,
+      { trustProxy: environment.trustProxy },
+      revokeSessionUseCase,
+      listSessionsUseCase,
+      revokeOtherSessionsUseCase,
+    );
+  }
+}
