bd7ecbb705cf821dd5104a6832a88244174fab33
Nguyen Ha Hiep <74894051+hiephanguyen01@users.noreply.github.com>
2026-08-07 11:04:25 +0700

    feat: provide tenant data session factory

--- apps/api/src/database/database.module.ts
+++ apps/api/src/database/database.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from "@nestjs/common";
 
+import { PrismaTenantDataSessionFactory } from "./prisma-tenant-data-session.factory.js";
 import { PrismaService } from "./prisma.service.js";
 
 @Global()
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [PrismaService, PrismaTenantDataSessionFactory],
+  exports: [PrismaService, PrismaTenantDataSessionFactory],
 })
 export class DatabaseModule {}
