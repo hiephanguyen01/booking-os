02053e82713f31d1fadd8bf9af72e12ec3ea9f7d
Nguyen Ha Hiep <74894051+hiephanguyen01@users.noreply.github.com>
2026-08-07 10:32:52 +0700

    feat: export authorization contracts

---
+++ packages/contracts/src/auth/index.ts
@@ -0,0 +1,7 @@
+export {
+  type AuthorizationContext,
+  AUTHORIZATION_PERMISSION_KEYS,
+  type AuthorizationPermissionKey,
+  AUTHORIZATION_ROLE_KEYS,
+  type AuthorizationRoleKey,
+} from "./authorization-context.js";
