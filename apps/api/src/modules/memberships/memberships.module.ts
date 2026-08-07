a4faecadcfdf6635de674686c0a82d8a83d51f9f
Nguyen Ha Hiep <74894051+hiephanguyen01@users.noreply.github.com>
2026-08-07 18:16:32 +0700

    feat: wire platform tenant provisioning use case

--- apps/api/src/modules/memberships/memberships.module.ts
+++ apps/api/src/modules/memberships/memberships.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 
 import { DatabaseModule } from "../../database/database.module.js";
+import { ProvisionTenantUseCase } from "./application/use-cases/provision-tenant.use-case.js";
 
 @Module({
   imports: [DatabaseModule],
+  providers: [ProvisionTenantUseCase],
 })
 export class MembershipsModule {}
