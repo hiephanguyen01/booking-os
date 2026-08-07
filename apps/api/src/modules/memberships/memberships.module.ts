f2606b25d2fbf4d9cc1d84053e2bb904d97c84b8
Nguyen Ha Hiep <74894051+hiephanguyen01@users.noreply.github.com>
2026-08-07 11:04:51 +0700

    feat: add memberships application module

---
+++ apps/api/src/modules/memberships/memberships.module.ts
@@ -0,0 +1,8 @@
+import { Module } from "@nestjs/common";
+
+import { DatabaseModule } from "../../database/database.module.js";
+
+@Module({
+  imports: [DatabaseModule],
+})
+export class MembershipsModule {}
