7f5ad29932a56122e0805bccf46d9e82ad205718
Nguyen Ha Hiep <74894051+hiephanguyen01@users.noreply.github.com>
2026-08-07 18:18:55 +0700

    revert: avoid unresolved provisioning DI wiring

--- apps/api/src/modules/memberships/memberships.module.ts
+++ apps/api/src/modules/memberships/memberships.module.ts
@@ -1,10 +1,8 @@
 import { Module } from "@nestjs/common";
 
 import { DatabaseModule } from "../../database/database.module.js";
-import { ProvisionTenantUseCase } from "./application/use-cases/provision-tenant.use-case.js";
 
 @Module({
   imports: [DatabaseModule],
-  providers: [ProvisionTenantUseCase],
 })
 export class MembershipsModule {}
