aebbf77057e4784edf59fc634ab06f6bf61c8839
Nguyen Ha Hiep <74894051+hiephanguyen01@users.noreply.github.com>
2026-08-18 11:03:09 +0700

    test: organize tenant role update imports

--- apps/api/src/modules/authorization/application/use-cases/tenant-rbac/update-tenant-custom-role.use-case.test.ts
+++ apps/api/src/modules/authorization/application/use-cases/tenant-rbac/update-tenant-custom-role.use-case.test.ts
@@ -5,7 +5,6 @@ import {
   TenantCustomRoleVersionConflictError,
   TenantRbacError,
 } from "../../../domain/tenant-rbac/tenant-rbac.errors.js";
-import { UpdateTenantCustomRoleUseCase } from "./update-tenant-custom-role.use-case.js";
 import {
   adminAuthorization,
   customRole,
@@ -15,6 +14,7 @@ import {
   ROLE_ID,
   USER_ID,
 } from "./tenant-rbac-use-case.test-fixtures.js";
+import { UpdateTenantCustomRoleUseCase } from "./update-tenant-custom-role.use-case.js";
 
 function updateHarness(current = customRole()) {
   const events: string[] = [];
